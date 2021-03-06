cfd54b621b05abf04dd23e973b6a437f3848cb26
Felipe Jung <felipemattoseu@gmail.com>
2021-03-06 11:57:54 -0400

    feat(about): create about page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 import dynamic from 'next/dynamic';
 
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline';
+
+import LinkWrapper from 'components/LinkWrapper';
 const Map = dynamic(() => import('components/Map'), { ssr: false });
 
-const Home = () => <Map />;
+const Home = () => (
+  <>
+    <LinkWrapper href="/about">
+      <InfoOutline size={32} aria-label="About" />
+    </LinkWrapper>
+
+    <Map />
+  </>
+);
 
 export default Home;
