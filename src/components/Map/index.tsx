13f71779759ae37b02830d74deaef84b58503eb4
Felipe Jung <felipemattoseu@gmail.com>
2021-03-06 16:00:22 -0400

    refactor(map): improve map zoom and experience

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/dist/client/router';
 
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet';
+
+import * as S from './styles';
 
 type Place = {
   id: string;
@@ -38,33 +40,55 @@ const Map = ({ places }: MapProps) => {
   const router = useRouter();
 
   return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={3}
-      style={{
-        height: '100%',
-        width: '100%'
-      }}
-    >
-      <CustomTileLayer />
+    <S.MapWrapper>
+      <MapContainer
+        center={[0, 0]}
+        zoom={3}
+        style={{
+          height: '100%',
+          width: '100%'
+        }}
+        minZoom={3}
+        maxBounds={[
+          [-180, 180],
+          [180, -180]
+        ]}
+      >
+        <MapConsumer>
+          {(map) => {
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth;
 
-      {places?.map(({ id, slug, name, location }) => {
-        const { latitude, longitude } = location;
+            if (width < 768) {
+              map.setMinZoom(2);
+            }
 
-        return (
-          <Marker
-            key={`place-${id}`}
-            position={[latitude, longitude]}
-            title={name}
-            eventHandlers={{
-              click: () => {
-                router.push(`/place/${slug}`);
-              }
-            }}
-          />
-        );
-      })}
-    </MapContainer>
+            return null;
+          }}
+        </MapConsumer>
+
+        <CustomTileLayer />
+
+        {places?.map(({ id, slug, name, location }) => {
+          const { latitude, longitude } = location;
+
+          return (
+            <Marker
+              key={`place-${id}`}
+              position={[latitude, longitude]}
+              title={name}
+              eventHandlers={{
+                click: () => {
+                  router.push(`/place/${slug}`);
+                }
+              }}
+            />
+          );
+        })}
+      </MapContainer>
+    </S.MapWrapper>
   );
 };
 
