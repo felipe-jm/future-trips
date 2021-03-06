0c261e37f4a9b632218eabef123532d13868c1c3
Felipe Jung <felipemattoseu@gmail.com>
2021-03-06 11:41:56 -0400

    test(map): ensure map component render dynamic locations via received props

---
+++ src/components/Map/test.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+
+import Map from '.';
+
+describe('<Map />', () => {
+  it('should render without any marker', () => {
+    render(<Map />);
+
+    expect(
+      screen.getByRole('link', {
+        name: /a js library for interactive maps/i
+      })
+    ).toBeInTheDocument();
+  });
+
+  it('should render with the marker in correct place', () => {
+    const place = {
+      id: '1',
+      name: 'Londres',
+      slug: 'londres',
+      location: {
+        latitute: 51.531025586772955,
+        longitude: -0.12836531048238128
+      }
+    };
+
+    const place2 = {
+      id: '2',
+      name: 'Tóquio',
+      slug: 'toquio',
+      location: {
+        latitute: 35.63043662093571,
+        longitude: 139.7625295321917
+      }
+    };
+
+    render(<Map places={[place, place2]} />);
+
+    expect(screen.getByTitle(/londres/i)).toBeInTheDocument();
+    expect(screen.getByTitle(/tóquio/i)).toBeInTheDocument();
+  });
+});
